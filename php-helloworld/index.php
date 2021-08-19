f9f8198788226a9770c34ffe2892e80c03285fba
Student User <student@workstation.lab.example.com>
2021-08-18 23:43:00 -0400

    deploy php app changes using Web console

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming!\n";
+print "Hello, World! php version is " . PHP_VERSION . " \n\n";
+print "A change is coming! \n";
+print "This change is from the web console"
 ?>
