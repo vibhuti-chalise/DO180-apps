7065959b4433923a062a86e05a320e4f137a62e8
Student User <student@workstation.lab.example.com>
2021-08-29 21:11:44 -0400

    here this goes

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,10 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming! \n";
-print "\n";
-print "Ja ja re, ja re ja re; Gandi naali mein jake tu muh dhoke aa; ja ja re ja re ja re;\n";
-print "\n";
-print "lets make another change \n";
 print "This change is from the web console"
 ?>
 
