fd7b4a83a9a00ef515b71c21d5bd8f47da13c370
Student User <student@workstation.lab.example.com>
2021-08-29 21:26:00 -0400

    hmm

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,7 +2,7 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming! \n";
 print "This change is from the web console"
-print
+print ""
 print "Kaun bhaati hari"
 ?>
 
